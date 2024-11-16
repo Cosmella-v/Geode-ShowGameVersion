7b2fedd9a0b92724418bdc18a019c5a0b98b3804
Neo Aylor <neoyukiaylor@gmail.com>
2024-11-16 11:04:17 +0000

    Revert prev commit as it breaks non windows builds

--- src/main.cpp
+++ src/main.cpp
@@ -13,8 +13,8 @@ class $modify(MenuLayer) {
         
 		// All I wanted to do was add a "v" before the game version but c++ is stupid and I hate it
 		char curGDVer[100];
-		strcpy_s(curGDVer, "v");
-		strcat_s(curGDVer, Loader::get()->getGameVersion().data());
+		strcpy(curGDVer, "v");
+		strcat(curGDVer, Loader::get()->getGameVersion().data());
 
 		auto verLabel = CCLabelBMFont::create(curGDVer, "bigFont.fnt");
 		
